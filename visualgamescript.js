6ab2aa27a25b1e66ea4bfb3a69f88dd5259a3166
butterdogg1467 <momink2600@gmail.com>
2024-10-17 22:16:57 -0400

    Created function to fetch cords between start and end points

--- visualgamescript.js
+++ visualgamescript.js
@@ -5,17 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
     let player = new Players()
     let computer = new Players(true)
 
+    let cordsdisplay = document.querySelector('.cordsdisplay')
+
     function createBoard(boardID) {
         let board = document.getElementById(boardID)
 
-        for (let i = 0; i < 10; i++){
+        for (let i = 9; i >=0; i--){
             for (let j = 0; j < 10; j++){
                 let cell = document.createElement('div')
-                cell.dataset.x = i
-                cell.dataset.y = j
+                cell.dataset.x = j
+                cell.dataset.y = i
                 cell.classList.add('cell')
                 board.appendChild(cell)
+                cell.addEventListener('mouseover', () => {
+                    cordsdisplay.textContent = '(' + cell.dataset.x + ', '+ cell.dataset.y + ')'
+                })
+
+                cell.addEventListener('mouseleave', () => {
+                    cordsdisplay.textContent = '(' + 0 + ', '+ 0 + ')'
+                })
+
+                cell.addEventListener('click', () => {
+                    console.log(cell.dataset)
+                })
             }
+        
         }
 
     }
@@ -23,7 +37,48 @@ document.addEventListener('DOMContentLoaded', () => {
     createBoard('playerboard')
     createBoard('computerboard')
 
-    player.board.placeShip([1,1], [1,3])
+    let shipCordsStart = [1,1]
+    let shipCordsEnd = [1,5]
+
+    player.board.placeShip(shipCordsStart, shipCordsEnd)
+
+    function middleCords(start, end) {
+        let yCords = []
+        let xCords = []
+
+        if (start[0] === end[0]) {
+
+            const constantX = start[0]
+            for (let i = Math.min(start[1], end[1]) + 1; i < Math.max(start[1], end[1]); i++) {
+                yCords.push([constantX, i])
+            }
+
+            return yCords
+
+        } else if (start[1] === end[1]) {
+
+            const constantY = start[1]
+            for (let i = Math.min(start[0], end[0]) + 1; i < Math.max(start[0], end[0]); i++) {
+                xCords.push([i, constantY ])
+            }
+
+            return xCords
+        }
+    }
+
+    middleCords(shipCordsStart, shipCordsEnd)
+
+    // function shipVisual(shipCordsStart, shipCordsEnd) {
+    //     cords.forEach(cord => {
+    //         let x = cord[1]
+    //         let y = cord[0]
+
+    //         let cell = document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+
+    //         cell.classList.add('ship')
+    //     });
+    // }
+    
 
 
 
