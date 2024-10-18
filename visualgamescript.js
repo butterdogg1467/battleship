003355db7fcf461db83ed67e8f7bf125e5a4f7b4
butterdogg1467 <momink2600@gmail.com>
2024-10-17 22:30:09 -0400

    Visualed ships correctly

--- visualgamescript.js
+++ visualgamescript.js
@@ -34,6 +34,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    function middleCords(start, end) {
+        let yCords = []
+        let xCords = []
+
+        if (start[0] === end[0]) {
+            yCords.push(start)
+
+            const constantX = start[0]
+            for (let i = Math.min(start[1], end[1]) + 1; i < Math.max(start[1], end[1]); i++) {
+                yCords.push([constantX, i])
+            }
+            yCords.push(end)
+
+            return yCords
+
+        } else if (start[1] === end[1]) {
+            xCords.push(start)
+
+            const constantY = start[1]
+            for (let i = Math.min(start[0], end[0]) + 1; i < Math.max(start[0], end[0]); i++) {
+                xCords.push([i, constantY])
+            }
+            xCords.push(end)
+
+            return xCords
+        }
+    }
+
+    function shipVisual(cords) {
+        cords.forEach(cord => {
+            let x = cord[1]
+            let y = cord[0]
+
+            let cell = document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+
+            cell.classList.add('ship')
+        });
+    }
+
+
     createBoard('playerboard')
     createBoard('computerboard')
 
@@ -42,43 +82,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     player.board.placeShip(shipCordsStart, shipCordsEnd)
 
-    function middleCords(start, end) {
-        let yCords = []
-        let xCords = []
+    computer.board.placeShip(shipCordsStart, shipCordsEnd)
 
-        if (start[0] === end[0]) {
-
-            const constantX = start[0]
-            for (let i = Math.min(start[1], end[1]) + 1; i < Math.max(start[1], end[1]); i++) {
-                yCords.push([constantX, i])
-            }
-
-            return yCords
-
-        } else if (start[1] === end[1]) {
-
-            const constantY = start[1]
-            for (let i = Math.min(start[0], end[0]) + 1; i < Math.max(start[0], end[0]); i++) {
-                xCords.push([i, constantY ])
-            }
-
-            return xCords
-        }
-    }
-
-    middleCords(shipCordsStart, shipCordsEnd)
-
-    // function shipVisual(shipCordsStart, shipCordsEnd) {
-    //     cords.forEach(cord => {
-    //         let x = cord[1]
-    //         let y = cord[0]
-
-    //         let cell = document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
-
-    //         cell.classList.add('ship')
-    //     });
-    // }
-    
+    shipVisual(middleCords(shipCordsStart, shipCordsEnd))
 
 
 
